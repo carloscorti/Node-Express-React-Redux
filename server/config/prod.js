eaff8ebc97eaf8972488c246a00c471f0aaa41a1
Carlos <carlosacorti@hotmail.com>
2020-07-13 18:49:18 +0200

    setting prod-dev variables

---
+++ server/config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoConnectionString: process.env.MONGO_URI,
+  sessionKey: process.env.SESSION_KEY,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+};
