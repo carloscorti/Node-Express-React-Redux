9ca8b5fcb3cf7106dad9a258f38f1982c2e3e01d
Carlos <carlosacorti@hotmail.com>
2020-07-15 18:47:27 +0200

    adding create-react-app font end
    -on client/src/setupProxy.js: setting proxi config for client side
    server matching server side server

--- server/config/strategies/google.oauth.strategy.js
+++ server/config/strategies/google.oauth.strategy.js
@@ -18,16 +18,44 @@ function oAuthStrategy() {
         callbackURL: googleOauthCallbackUri,
       },
 
-      (accessToken, refreshToken, profile, done) => {
-        // debug(`Acces token: ${accessToken}`);
-        // debug(`Refresh Token: ${refreshToken}`);
-        // debug(`Profile: ${profile}`);
-        User.findOne({ providerId: profile.id }, (err, user) => {
-          if (err) {
-            return done(err, { message: 'Ups there was an error' });
-          }
+      // (accessToken, refreshToken, profile, done) => {
+      //   // debug(`Acces token: ${accessToken}`);
+      //   // debug(`Refresh Token: ${refreshToken}`);
+      //   // debug(`Profile: ${profile}`);
+      //   User.findOne({ providerId: profile.id }, (err, user) => {
+      //     if (err) {
+      //       return done(err, { message: 'Ups there was an error' });
+      //     }
+      //     if (!user) {
+      //       user = new User({
+      //         providerId: profile.id,
+      //         name: profile.displayName,
+      //         email: profile.emails[0].value,
+      //         username: profile.username,
+      //         provider: profile.provider,
+      //         google: profile._json,
+      //       });
+      //       user.save((err) => {
+      //         if (err) {
+      //           debug(err);
+      //           return done(err, { message: 'Ups try loggin again' });
+      //         }
+      //         return done(null, user);
+      //       });
+      //     } else {
+      //       return done(null, user);
+      //     }
+      //   });
+      // }
+      //     )
+      //   );
+      // }
+
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          const user = await User.findOne({ providerId: profile.id });
           if (!user) {
-            user = new User({
+            const newUser = new User({
               providerId: profile.id,
               name: profile.displayName,
               email: profile.emails[0].value,
@@ -35,49 +63,21 @@ function oAuthStrategy() {
               provider: profile.provider,
               google: profile._json,
             });
-            user.save((err) => {
-              if (err) {
-                debug(err);
-                return done(err, { message: 'Ups try loggin again' });
-              }
-              return done(null, user);
-            });
-          } else {
-            return done(null, user);
+            const mongoNewUser = await newUser.save(); //((err) => {
+            // if (err) {
+            //   debug(err);
+            //   return done(err, { message: 'Ups try loggin again' });
+            // }
+            return done(null, mongoNewUser);
           }
-        });
+          return done(null, user);
+        } catch (err) {
+          debug(err);
+          return done(err, { message: 'Ups there was an error' });
+        }
       }
     )
   );
 }
-
-//     async (accessToken, refreshToken, profile, done) => {
-//       try {
-//         const user = await User.findOne({ providerId: profile.id });
-//         if (!user) {
-//           const newUser = new User({
-//             providerId: profile.id,
-//             name: profile.displayName,
-//             email: profile.emails[0].value,
-//             username: profile.username,
-//             provider: profile.provider,
-//             google: profile._json,
-//           });
-//           const mongoNewUser = await newUser.save; //((err) => {
-//           // if (err) {
-//           //   debug(err);
-//           //   return done(err, { message: 'Ups try loggin again' });
-//           // }
-//           return done(null, mongoNewUser);
-//         }
-//         return done(null, user);
-//       } catch (err) {
-//         debug(err);
-//         return done(err, { message: 'Ups there was an error' });
-//       }
-//     }
-//   )
-// );
-// }
 
 module.exports = oAuthStrategy;
