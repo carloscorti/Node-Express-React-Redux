4f1e66b6d795d924edb6aa9599eb5d872ae8ba3a
Carlos <carlosacorti@hotmail.com>
2020-07-20 17:29:13 +0200

    setting app redux on react side
    -on client/src/reducers/authReducer.js: setting up auth reducer
    -on client/src/reducers/index.js: it's called index.js to imported when
    somewhere it's imported the reducers folder (redux convenion), exports
    combineReducer with all the reducres as a value-key object
    -on client/src/index.js: initialize store, wraps app with provider and
    pass store as provider props, reducers is imported from
    client/src/reducers/index.js when imports the whole reducers folder

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,10 @@
+const { STATES } = require('mongoose');
+
+const authReducer = (store = {}, action) => {
+  switch (action.type) {
+    default:
+      return store;
+  }
+};
+
+export default authReducer;
