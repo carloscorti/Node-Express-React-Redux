10eca84023bd37a46a17f1c11976d9f79d22ecf0
Carlos <carlosacorti@hotmail.com>
2020-07-21 17:06:54 +0200

    setting up react router
    -on client/src/components/App.js: setting up app routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const DashBoard = () => <h2>DashBoard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <h1>Hellow</h1>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" component={Landing} exact />
+          <Route path="/surveys" component={DashBoard} exact />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
