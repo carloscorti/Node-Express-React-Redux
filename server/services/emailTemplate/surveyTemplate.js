8360511442a87c65a0703b8887ad5e0fd2c31084
Carlos <carlosacorti@hotmail.com>
2020-09-24 16:09:56 +0200

    improving email template
    -on server/services/emailTemplate/surveyTemplate.js: improving mail body

--- server/services/emailTemplate/surveyTemplate.js
+++ server/services/emailTemplate/surveyTemplate.js
@@ -1,4 +1,20 @@
 const surveyTemplate = ({ body }) => {
-  return `<div>${body}</div>`;
+  return `
+  <html>
+    <body>
+      <div style="text-align: center;">
+        <h3>I'd like your input!</h3>
+        <p>Please answer the following question:</p>
+        <p>${body}</p>
+        <div>
+          <a href="http://localhost:3000">Yes</a>
+        </div>
+        <div>
+          <a href="http://localhost:3000">No</a>
+        </div>
+      </div>
+    </body>
+  </html>
+`;
 };
 module.exports = surveyTemplate;
